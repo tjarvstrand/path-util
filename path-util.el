004bf2aedfc61c92e564c3f7468a93d875e8545f
Thomas Järvstrand <thomas.jarvstrand@klarna.com>
2013-01-12 14:26:56 +0100

    Add path-util.el

---
+++ path-util.el
@@ -0,0 +1,18 @@
+;; Copyright 2013 Thomas Järvstrand <tjarvstrand@gmail.com>
+;;
+;; This file is part of path-util.
+;;
+;; path-util is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Lesser General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; path-util is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with path-util. If not, see <http://www.gnu.org/licenses/>.
+;;
+;; Path handling utilities
